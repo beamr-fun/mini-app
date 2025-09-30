b2b09a0721228a4b0928f7b2a73942d0efd5ad12
jordan <jordan.lesich@gmail.com>
2025-09-30 14:59:03 -0700

    scaffold onboard context

--- src/context/OnboardContext.tsx
+++ src/context/OnboardContext.tsx
@@ -1,19 +1,32 @@
 import React, { ReactNode } from 'react';
+import { useForm, UseFormReturnType } from '@mantine/form';
 
 type OnboardContextType = {
   budget: number;
+  preferredAddress: string;
+  form?: UseFormReturnType<OnboardContextType>;
 };
 
 export const OnboardContext = React.createContext<OnboardContextType>({
   budget: 0,
+  preferredAddress: '',
+  form: undefined,
 });
 
 export const OnboardDataProvider = ({ children }: { children: ReactNode }) => {
-  const [budget, setBudget] = React.useState(0);
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      preferredAddress: '',
+      budget: 0,
+    },
+  });
   return (
     <OnboardContext.Provider
       value={{
-        budget,
+        budget: form.values.budget,
+        preferredAddress: form.values.preferredAddress,
+        form: form,
       }}
     >
       {children}
