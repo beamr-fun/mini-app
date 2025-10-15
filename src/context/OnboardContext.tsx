bb01769e4de861f5a84270482ecb948720c5edf9
jordan <jordan.lesich@gmail.com>
2025-10-15 10:48:33 -0700

    hook live addresses up to UI, hook balance up to UI

--- src/context/OnboardContext.tsx
+++ src/context/OnboardContext.tsx
@@ -1,41 +1,51 @@
 import React, { ReactNode } from 'react';
 import { useForm, UseFormReturnType } from '@mantine/form';
 import { useQuery } from '@tanstack/react-query';
+import { Follower } from '@neynar/nodejs-sdk/build/api';
+import { ADDR } from '../const/addresses';
+import { useAccount, useReadContract, useReadContracts } from 'wagmi';
+import { erc20Abi } from 'viem';
+import { useUser } from '../hooks/useUser';
 
 type OnboardContextType = {
   budget: number;
   preferredAddress: string;
   form?: UseFormReturnType<OnboardContextType>;
+  balance?: bigint;
 };
 
 export const OnboardContext = React.createContext<OnboardContextType>({
   budget: 0,
   preferredAddress: '',
   form: undefined,
+  balance: undefined,
 });
 
 const fetchUserFollowing = async (fid: number) => {
-  const res = await fetch(`http://localhost:3000/v1/user/following/${fid}/al`);
+  console.log('fetched');
+  const res = await fetch(`http://localhost:3000/v1/user/following/${fid}/all`);
   const data = await res.json();
   if (!res.ok) {
     throw new Error(data?.error || 'Failed to fetch user following');
   }
 
-  return data.flatmap();
+  return data.following.flat() as Follower[];
 };
 
 export const OnboardDataProvider = ({ children }: { children: ReactNode }) => {
-  const fid = 11650;
+  const { user } = useUser();
 
   const {
     data: userFollowing,
     error,
     isLoading,
   } = useQuery({
-    queryKey: ['userFollowing', fid],
-    queryFn: () => fetchUserFollowing(fid),
-    enabled: !!fid,
+    queryKey: ['userFollowing', user?.fid],
+    queryFn: () => fetchUserFollowing(user!.fid),
+    enabled: !!user?.fid,
   });
+
+  const { address } = useAccount();
 
   const form = useForm({
     mode: 'controlled',
@@ -45,12 +55,26 @@ export const OnboardDataProvider = ({ children }: { children: ReactNode }) => {
     },
   });
 
+  console.log('form.values.preferredAddress', form.values.preferredAddress);
+  const { data: userBalance } = useReadContract({
+    address: ADDR.SUPER_TOKEN,
+    abi: erc20Abi,
+    functionName: 'balanceOf',
+    query: {
+      enabled: !!form.values.preferredAddress,
+    },
+    args: [form.values.preferredAddress as `0x${string}`],
+  });
+
+  console.log('userBalance', userBalance);
+
   return (
     <OnboardContext.Provider
       value={{
         budget: form.values.budget,
         preferredAddress: form.values.preferredAddress,
         form: form,
+        balance: userBalance,
       }}
     >
       {children}
--- src/pages/Budget.tsx
+++ src/pages/Budget.tsx
@@ -17,23 +17,26 @@ import { generateRandomAddress, truncateAddress } from '../utils/common';
 import { useNavigate } from 'react-router-dom';
 import { useOnboard } from '../hooks/useOnboard';
 import { Tag } from '../components/Tag';
-
-const DUMMY_ADDRESSES = Array.from({ length: 5 }).map((_, index) => {
-  const address = generateRandomAddress();
-  return {
-    value: address,
-    label: truncateAddress(address),
-    isPrimaryAddress: index === 2,
-    isCurrentAddress: index === 0,
-  };
-});
+import { useUser } from '../hooks/useUser';
+import { useAccount } from 'wagmi';
+import { formatEther } from 'viem';
 
 export const Budget = () => {
+  const { user } = useUser();
+  const { balance } = useOnboard();
+  console.log('balance', balance);
   const theme = useMantineTheme();
   const navigate = useNavigate();
   const { form } = useOnboard();
+  const { address } = useAccount();
 
   if (!form) return null;
+
+  console.log('address', address);
+
+  const formattedBalance = balance
+    ? Number(formatEther(balance)).toFixed(2)
+    : '0';
 
   return (
     <Box>
@@ -47,12 +50,21 @@ export const Budget = () => {
         description="Address cannot be changed for now"
         label="Wallet Address"
         placeholder="0xb3amr..."
-        data={DUMMY_ADDRESSES}
+        defaultValue={address || undefined}
+        data={user?.eth_addresses.map((addr) => ({
+          label: truncateAddress(addr),
+          value: addr,
+          isPrimaryAddress:
+            addr.toLocaleLowerCase() ===
+            user?.primary_address?.toLocaleLowerCase(),
+          isCurrentAddress:
+            addr.toLocaleLowerCase() === address?.toLocaleLowerCase(),
+        }))}
         mb={40}
         key={form.key('preferredAddress')}
         renderOption={(item: any) => {
           return (
-            <Group>
+            <Group gap={'sm'}>
               <Text>{item.option.label}</Text>
               {item.option.isPrimaryAddress && (
                 <Tag c="var(--mantine-color-gray-0)" fw={500}>
@@ -71,7 +83,7 @@ export const Budget = () => {
       />
       <Stack align="center" gap={2} mb={48}>
         <Group gap="sm" align="end">
-          <Text fz={36}>823.12k</Text>
+          <Text fz={36}>{formattedBalance}</Text>
           <Text
             fz={'sm'}
             variant="label"
@@ -82,9 +94,9 @@ export const Budget = () => {
             BEAMR
           </Text>
         </Group>
-        <Tooltip label={'Net flow rate (incoming + outgoing) '}>
+        {/* <Tooltip label={'Net flow rate (incoming + outgoing) '}>
           <Text c={theme.colors.purple[6]}>-25,293.29 BEAMR/mo</Text>
-        </Tooltip>
+        </Tooltip> */}
       </Stack>
       <Box mb={48}>
         <Text variant="label">Monthly Budget</Text>
@@ -95,7 +107,7 @@ export const Budget = () => {
           rightSection={'BEAMR'}
           rightSectionWidth={70}
           key={form.key('budget')}
-          description={'Your current balance is 823.12k'}
+          description={`Your current balance is ${formattedBalance}`}
           {...form.getInputProps('budget')}
         />
       </Box>
